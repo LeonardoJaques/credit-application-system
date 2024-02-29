102a15a696172cd9d0b9084a49a9692faaa4a8a4
Leonardo Jaques <jaques.projetos@outlook.com>
2024-02-29 12:00:12 -0300

    feat: create layer of entity and respository

---
+++ src/main/kotlin/dev/leonardo/requestcreditsystem/repository/CreditRepository.kt
@@ -0,0 +1,8 @@
+package dev.leonardo.requestcreditsystem.repository
+
+import dev.leonardo.requestcreditsystem.entity.Credit
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CreditRepository: JpaRepository<Credit, Long>
